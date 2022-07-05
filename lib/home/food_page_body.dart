808de2fe0843bc64383e958f4cfca8022cafbea3
Junior Nogueira <frjuniornogueira1@gmail.com>
2022-07-05 16:26:38 -0300

    add os Dots inidadores da pagina

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,3 +1,4 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/colors.dart';
@@ -15,94 +16,172 @@ class FoodPageBody extends StatefulWidget {
 class _FoodPageBodyState extends State<FoodPageBody> {
 
   PageController pageController = PageController(viewportFraction: 0.85);
+  //variavel que crola o valor da pagina no zoom
+  var _currPageValue=0.0;
+  double _scaleFactor=0.8;
+  double _height=220;
+  @override
+  void initState (){
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue = pageController.page!; 
+        // print("Valor corrente "+_currPageValue.toString());
+      });     
+    });
+  }
+
+  @override
+  void dispose(){
+    pageController.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Column(
+      children: [
+        Container(
       // color: Colors.red,
-      height: 320,
-      child: PageView.builder(
-        controller: pageController,
-        itemCount: 5,
-        itemBuilder: (context, position){
-        return _buildPageItem(position);
-      }),
-      
+          height: 320,
+          child: PageView.builder(
+            controller: pageController,
+            itemCount: 5,
+            itemBuilder: (context, position){
+            return _buildPageItem(position);
+          }),
+        ),
+        new DotsIndicator(
+          dotsCount: 5,
+          position: _currPageValue,
+          decorator: DotsDecorator(
+            activeColor: AppColors.mainColor,
+            size: const Size.square(9.0),
+            activeSize: const Size(18.0, 9.0),
+            activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+          ),
+        )        
+      ],
     );
   }
 
+
+
   Widget _buildPageItem (int index){
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: EdgeInsets.only(left: 10, right: 10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage(
-                "assets/image/food2.png"
-              )
-            )
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 120,
-            margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+    //trabalhando o zoom usando a funcao intstate
+    Matrix4 matrix = new Matrix4.identity();
+    if (index == _currPageValue.floor()){
+      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+
+    }else if (index == _currPageValue.floor()+1){
+      var currScale = _scaleFactor+(_currPageValue-index+1)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }else if (index == _currPageValue.floor()-1){
+      var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
+      var currTrans = _height*(1-currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }else{
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height*(1-_scaleFactor)/2, 1);
+    }
+
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: EdgeInsets.only(left: 10, right: 10),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
-              color: Colors.white,
-            ),
-            child: Container(
-              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BigText(text: "Comida Chinesa"),
-                  SizedBox(height: 10,),
-                  Row(
-                    children: [
-                      Wrap(
-                        // arrumar as estrelas para vim do banco
-                        children: List.generate(5, (index)  { return Icon(Icons.star, color: AppColors.mainColor, size: 15,);}),
-                      ),
-                      SizedBox(width: 10,),
-                      SmallText(text: "4.5"),
-                      SizedBox(width: 10,),
-                      SmallText(text: "1287"),
-                      SizedBox(width: 10,),
-                      SmallText(text: "Comentarios"),
-                    ],
-                  ),
-                  SizedBox( height: 20,),
-                  Row(
-                    children: [
-                      IconAndTextWidget(icon: Icons.circle_sharp, 
-                        text: "Normal",
-                        IconColor: AppColors.iconColor1
-                      ),
-
-                      IconAndTextWidget(icon: Icons.location_on, 
-                        text: "1.8km",
-                        IconColor: AppColors.mainColor
-                      ),
-
-                      IconAndTextWidget(icon: Icons.access_time_rounded, 
-                        text: "38min",
-                        IconColor: AppColors.iconColor2
-                      )
-                    ],
-                  )
-                ],
-              ),
+              color: index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  "assets/image/food2.png"
+                )
+              )
             ),
           ),
-        )
-      ],
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFFe8e8e8),
+                    blurRadius: 5.0,
+                    offset: Offset(0, 5)
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-5, 0)
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(5, 0)
+                  )
+                ]
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Comida Chinesa"),
+                    SizedBox(height: 10,),
+                    Row(
+                      children: [
+                        Wrap(
+                          // arrumar as estrelas para vim do banco
+                          children: List.generate(5, (index)  { return Icon(Icons.star, color: AppColors.mainColor, size: 15,);}),
+                        ),
+                        SizedBox(width: 10,),
+                        SmallText(text: "4.5"),
+                        SizedBox(width: 10,),
+                        SmallText(text: "1287"),
+                        SizedBox(width: 10,),
+                        SmallText(text: "Comentarios"),
+                      ],
+                    ),
+                    SizedBox( height: 20,),
+                    Row(
+                      children: [
+                        IconAndTextWidget(icon: Icons.circle_sharp, 
+                          text: "Normal",
+                          IconColor: AppColors.iconColor1
+                        ),
+    
+                        IconAndTextWidget(icon: Icons.location_on, 
+                          text: "1.8km",
+                          IconColor: AppColors.mainColor
+                        ),
+    
+                        IconAndTextWidget(icon: Icons.access_time_rounded, 
+                          text: "38min",
+                          IconColor: AppColors.iconColor2
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+
+            ),
+          )
+        ],
+      ),
     );
   }
 
