3160c7fdf9f3d060bc1f771f5723b9ff40119cb2
Junior Nogueira <frjuniornogueira1@gmail.com>
2022-07-21 14:05:02 -0300

    add API cliente, repositorio e controladores no arquivo dependencies

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,5 +1,15 @@
 
+import 'package:food_delivery/controllers/popular_product_controller.dart';
+import 'package:food_delivery/data/api/api_client.dart';
+import 'package:food_delivery/data/repository/popular_product_repo.dart';
+import 'package:get/get.dart';
+
 Future<void> init()async{
-  
-  
+  // API cliente
+  Get.lazyPut(()=>ApiClient(appBaseUrl: "https://algumacoisa"));
+  //repositorio
+  Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+  //controladores
+  Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
+ 
 }
