d2398f0376c88112c939672ea7343173daab4941
Junior Nogueira <frjuniornogueira1@gmail.com>
2022-08-22 08:44:24 -0300

    pegando informacoes recomendadas

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -4,6 +4,9 @@ import 'package:food_delivery/data/repository/popular_product_repo.dart';
 import 'package:get/get.dart';
 import 'package:food_delivery/utils/app_constants.dart';
 
+import '../controllers/recommended_product_controller.dart';
+import '../data/repository/recommended_product_repo.dart';
+
 
 Future<void> init() async {
   // api client
@@ -11,7 +14,10 @@ Future<void> init() async {
 
   // repos
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+  Get.lazyPut(() => RecommendedProductRepo(apiClient: Get.find()));
 
   // controllers
   Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
+  Get.lazyPut(() => RecommendedProductController(recommendedProductRepo: Get.find()));
+  
 }
